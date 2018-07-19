c4372fa3068abc3b5da73ef5039fb1cd2bfbf5e6
Sean Hamilton <seanh169@gmail.com>
2018-07-19 09:52:39 -0400

    mongoose changes

--- MEAN/Mongoose/restfulTaskAPI/hello-angular/src/app/app.component.ts
+++ MEAN/Mongoose/restfulTaskAPI/hello-angular/src/app/app.component.ts
@@ -8,18 +8,53 @@ import { HttpService } from './http.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  tasks; 
-  title = 'MEAN';
+  tasks = [];
+  title = 'Restful Tasks API';
   constructor(private _httpService: HttpService){}
 
-  ngOnInit(){
+    ngOnInit(){
       this.getTasksFromService();
       // console.log("Hello");
     }
     getTasksFromService(){
-    let observable = this._httpService.getTasks();
-    observable.subscribe(data => console.log("Got our tasks!", data));
-    this.tasks = data; 
+      let observable = this._httpService.getTasks();
+
+      observable.subscribe(data => {
+
+      console.log("Got our tasks!", data)
+      for(let dat in data){ 
+        this.tasks.push((data[dat]));
+      }
+      console.log(this.tasks);
+   
+    });
+
+    
+}
+onButtonClickParam(num: Number): void { 
+    console.log(`Click event is working with num param: ${num}`);
+    // call the service's method to post the data, but make sure the data is bundled up in an object!
+    let observable = this._httpService.postToServer({data: num});
+    observable.subscribe(data => console.log("Got our data!", data));
+
+
+
+}
+onButtonClick(): void {
+ let observable = this._httpService.getTasks();
+ let tasks = document.getElementById("tasks");
+
+ observable.subscribe(data => {
+     console.log(data);
+     for(let dat in data){ 
+       document.getElementById("tasks").innerHTML ="<b>task title</b>" + "<br><br>" +  data[dat].title + "-" + data[dat].description;
+
+
+     }
+
+
+ })
+
 }
 
 }
--- MEAN/Mongoose/restfulTaskAPI/hello-angular/src/app/http.service.ts
+++ MEAN/Mongoose/restfulTaskAPI/hello-angular/src/app/http.service.ts
@@ -17,12 +17,18 @@ export class HttpService {
  //    // subscribe to the Observable and provide the code we would like to do with our data from the response
  //    tempObservable.subscribe(data => console.log("Got our tasks!", data));
  // 	}
-  constructor(private _http: HttpClient) { 
+  	constructor(private _http: HttpClient) { 
   	// console.log(this._http.get('/'));
   	// this.getTasks();
 
-  }
- getTasks() {
- 	return this._http.get('/tasks');
- }
+  	}
+	 getTasks() {
+ 		return this._http.get('/tasks');
+	 }
+ 	postToServer(num){
+    // use the .post() method of HttpClient
+    // num must be an object
+    // provide the url of your post route - make sure this is set up in your server!
+    return this._http.post('/tasks', num);  
+	}
 }
