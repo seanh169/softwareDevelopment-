bbbc3a47fe58f4b264fd7d649671d184a59e8ff6
Sean Hamilton <seanh169@gmail.com>
2018-10-29 12:00:03 -0400

    VueJS Quotes Application

---
+++ VueJS/README.md
@@ -0,0 +1,1 @@
+This is a repository for sample VueJS applications I created while learning this framework! If you have any questions - please feel free to contact me at seanh169@gmail.com. Thanks!
--- eclipseApplications/HelloWorld/src/HelloWorld.java
+++ eclipseApplications/HelloWorld/src/HelloWorld.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
 
 public class HelloWorld {
-	static String a = new String("Hello");
+	
 	public static void main(String[] args) {
-//		Scanner in = new Scanner(in);
-		
-		System.out.println(a.hashCode());
-		String b = new String("Hello");
-		System.out.println(b.hashCode());
-		
-		System.out.println(a.equals(b));
-		System.out.println(a);
+		int varA = 20;
+		intresults = varAA+;
+		System.out.println(intresults);
+		System.out.println("Hello World");
 	}
 }
